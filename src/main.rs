0b5b92a6e50fe813a67fc9a9ab5005f49611795e
Boris <borisimo.work@gmail.com>
2024-05-22 01:38:13 +0300

    add: telegram api act, library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod tg_client;
+use rust_tdlib::{
+    tdjson,
+};
+use crate::tg_client::TgClient;
+
+#[tokio::main]
+async fn main() {
+    tdjson::set_log_verbosity_level(1);
+    env_logger::init();
+
+    let tg = TgClient::new(std::env::var("API_ID").unwrap().parse::<i32>().unwrap(), std::env::var("API_HASH").unwrap(), "group").await;
+    let v = tg.get_metafiles();
+
+    for vec in v {
+        println!("Meta: {}   ID: {}", vec.meta, vec.id);
+    }
+
+    tg.stop().await;
 }
